9f9b440e066a5807984365da7a2c15dd54737a62
Rumu1027 <afifafatema@gmail.com>
2020-07-21 22:01:18 -0500

    first commit

---
+++ src/helloWorld/Hello.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		String s = "Sam";
+		b = a;
+		a=a+a;
+		
+		
+		System.out.println(a+s);
+}
+}
